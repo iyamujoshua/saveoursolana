1f0ad68408b110ff96d21abde285aeb83e08a6c2
iyamujoshua <joshamu20@gmail.com>
2024-06-12 22:21:47 +0100

    Tokenomics

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -3,11 +3,13 @@ import Header from '../Header'
 import Presale from '../presale'
 import Footer from '../Footer'
 import Ect from "../Ect"
+import Tokenomics from '../Tokenomics'
 const Body = () => {
   return (
     <div>
         <Header/>
         <Presale/>
+        <Tokenomics/>
         <Ect/>
         <Footer/>
     </div>
---
+++ src/components/Tokenomics/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css"
+// import img1 from '../../assets/circleblur4.png'
+export default function Tokenomics() {
+  return (
+    <div id="Tokenomics" className="Tokenomics">
+      {/* <img src={img1} alt="" className=""/> */}
+      <div className="tokenomics-contents">
+        <h1>TOKENOMICS</h1>
+        {/* <p>
+          Introducing $SLOTS token, our native currency provides players with <br />
+          unmatched utility and value compared to fiat money and other <br />
+          crypotcurrencies. $SLOTS is an ERC20 token that <br /> runs on the blockchain
+        </p> */}
+        {/* <button className="Buy">
+              $Buy $SLOT
+            </button> */}
+      </div>
+    </div>
+  );
+}
