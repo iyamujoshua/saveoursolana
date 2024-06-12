b279481e6b82c0cec64a9d4bb279ca7c2f1af2c7
iyamujoshua <joshamu20@gmail.com>
2024-06-12 22:14:13 +0100

    added Ect section

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../Header'
 import Presale from '../presale'
 import Footer from '../Footer'
+import Ect from "../Ect"
 const Body = () => {
   return (
     <div>
         <Header/>
         <Presale/>
+        <Ect/>
         <Footer/>
     </div>
   )
---
+++ src/components/Ect/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./style.css"
+export default function Ect() {
+  return (
+    <div className='Features'>
+        <div className="Features_flex">
+        <div className="Features_container1">
+          <h3>
+          0%
+          </h3>
+          <p>
+          BUY TAX
+          </p>
+        </div>
+        <div className="Features_container1">
+          <h3>
+          0%
+          </h3>
+          <p>
+          SELL FAR
+          </p>
+        </div>
+
+        <div className="Features_container1">
+          <h3>
+          100%
+          </h3>
+          <p>
+          BURNED LIQUIDITY
+          </p>
+        </div>
+        <div className="Features_container1">
+          <h3>
+          100M
+          </h3>
+          <p>
+          TOTAL SUPPLY
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
